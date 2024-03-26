0f2e7378a4de6f637d934cdfa2e928233ecbecca
Prakhar7755 <prakhar.5779.aps3@gmail.com>
2024-03-26 08:39:32 +0530

    Weather app part-3

--- 7 Weather App/src/components/WeatherApp.jsx
+++ 7 Weather App/src/components/WeatherApp.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherApp.css";
 import search_icon from "../assets/search.png";
 import cloud_icon from "../assets/cloud.png";
 import humidity_icon from "../assets/humidity.png";
 import wind_icon from "../assets/wind.png";
+import clear_icon from "../assets/clear.png";
+import drizzle_icon from "../assets/drizzle.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
 
 const WeatherApp = () => {
+  const [weatherIcon, setWeatherIcon] = useState(cloud_icon);
   let api_key = "dd94f859a0e52d6e4767fddf735f04a7";
 
   const search = async () => {
     const element = document.getElementsByClassName("cityInput");
-    if ((element[0].value = "")) {
+    if (element[0].value === "") {
       return 0;
     }
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
@@ -23,10 +28,36 @@ const WeatherApp = () => {
     let temperature = document.getElementsByClassName("weather-temp");
     let location = document.getElementsByClassName("weather-location");
 
-    humidity[0].innerHTML = data.main.humidity;
-    wind[0].innerHTML = data.wind.speed;
-    temperature[0].innerHTML = data.main.temp;
+    humidity[0].innerHTML = data.main.humidity + " %";
+    wind[0].innerHTML = data.wind.speed + " km/hr";
+    temperature[0].innerHTML = data.main.temp + " °c";
     location[0].innerHTML = data.name;
+
+    if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
+      setWeatherIcon(clear_icon);
+    } else if (
+      data.weather[0].icon === "02n" ||
+      data.weather[0].icon === "02d"
+    ) {
+      setWeatherIcon(cloud_icon);
+    } else if (
+      data.weather[0].icon === "03n" ||
+      data.weather[0].icon === "03d"
+    ) {
+      setWeatherIcon(drizzle_icon);
+    } else if (
+      data.weather[0].icon === "13n" ||
+      data.weather[0].icon === "13d"
+    ) {
+      setWeatherIcon(snow_icon);
+    } else if (
+      data.weather[0].icon === "10d" ||
+      data.weather[0].icon === "10n"
+    ) {
+      setWeatherIcon(rain_icon);
+    }
+
+    
   };
 
   return (
@@ -39,10 +70,11 @@ const WeatherApp = () => {
       </div>
 
       <div className="weather-image">
-        <img src={cloud_icon} alt="" />
+        <img src={weatherIcon} alt="" />
       </div>
       <div className="weather-temp">24 °C</div>
       <div className="weather-location">London</div>
+
       <div className="data-container">
         <div className="element">
           <img src={humidity_icon} alt="" className="icon" />
@@ -64,3 +96,9 @@ const WeatherApp = () => {
 };
 
 export default WeatherApp;
+
+/* 
+50n smoke
+50d haze
+04n clouds
+ */
